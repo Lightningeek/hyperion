2a89472d6ea7fd1014a8133062cec817f2bd083e
Yanjia Ji <117481654+Lightningeek@users.noreply.github.com>
2024-05-09 11:28:18 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 player.onItemInteracted(IRON_SWORD, function () {
-    player.teleport(posCamera(0, 0, 8))
-    mobs.spawn(EXPLOSION_HUGE, posCamera(0, 0, 8))
     player.tell(mobs.target(LOCAL_PLAYER), "You have used Wither Impact for 400 Mana")
+    player.teleport(posCamera(0, player.getOrientation(), 8))
+    mobs.spawn(EXPLOSION_HUGE, posCamera(0, 0, 8))
     mobs.spawn(PRIMED_TNT, posCamera(0, 0, 8))
 })
