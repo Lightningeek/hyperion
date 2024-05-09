487a6935d4648cafd5055bc385c50be56abcfe41
Yanjia Ji <117481654+Lightningeek@users.noreply.github.com>
2024-05-09 11:24:57 -0700

    q:

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
 player.onItemInteracted(IRON_SWORD, function () {
-    mobs.spawn(EXPLOSION_HUGE, posCamera(0, 0, 0))
+    player.teleport(posCamera(0, 0, 8))
+    mobs.spawn(EXPLOSION_HUGE, posCamera(0, 0, 8))
+    player.tell(mobs.target(LOCAL_PLAYER), "You have used Wither Impact for 400 Mana")
+    mobs.spawn(PRIMED_TNT, posCamera(0, 0, 8))
 })
